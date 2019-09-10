ee02f8003de80d25b175c3c363e528fc54c9dce9
Bishwambhar Sen <bishwambhar.sen@gmail.com>
2019-09-10 22:44:51 +0800

    Added LDS for Contact creation

--- force-app/main/default/aura/FileUploader/FileUploaderController.js
+++ force-app/main/default/aura/FileUploader/FileUploaderController.js
@@ -2,5 +2,9 @@
     handleUploadFinished: function(cmp, evt, helper) {
         let uploadedFiles = event.getParam('files');
         console.log('Files Uploaded: ', JSON.stringify(uploadedFiles));
+    },
+
+    handleRecordUpdated: function(cmp, evt, helper) {
+
     }
 })
